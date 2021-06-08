be8a19428e7f8f32c13a5bb8d062a457e9987579
jasonlee <jlee7060@gmail.com>
2021-06-07 20:38:29 -0400

    frequence histogram fro base quality cluster

---
+++ handlingFASTQ.py
@@ -0,0 +1,63 @@
+import math
+import wget
+import matplotlib.pyplot as plt
+# import numpy as np
+def downloadFile(url):
+    filename = wget.download(url)
+    print("{} has been downloaded".format(filename))
+    return filename
+
+
+def QtoPhred33(Q):
+    return (int(math.ceil(Q)) + 33)
+
+
+def phred33toQ(C):
+    return ord(C) - 33
+
+
+def readFastq(filename):
+    sequence = []
+    qualities = []
+    with open(filename) as filehandle:
+        while True:
+            filehandle.readline()
+            seq = filehandle.readline().rstrip()
+            filehandle.readline()
+            qual = filehandle.readline().rstrip()
+            if len(seq) == 0:
+                break
+            sequence.append(seq)
+            qualities.append(qual)
+    return sequence, qualities
+
+
+# it gives frequency of quality base
+def createHist(qualities):
+    hist = [0] * 50
+    for qual in qualities:
+        for phred in qual:
+            q = phred33toQ(phred)
+            hist[q] += 1
+    return hist
+
+
+def main():
+    url = "http://d28rh4a8wq0iu5.cloudfront.net/ads1/data/SRR835775_1.first1000.fastq"
+    # downloadFile(url)
+    filename = "SRR835775_1.first1000.fastq"
+    seqs, quals = readFastq(filename)
+    print(seqs[:5])
+    print(quals[:5])
+    # print(phred33toQ('@'))
+    lst = createHist(quals)
+    print(lst)
+    print(range(50))
+    plt.bar(range(len(lst)),lst)
+    plt.show()
+
+
+
+
+if __name__ == '__main__':
+    main()
--- randomSeq.py
+++ randomSeq.py
@@ -3,7 +3,7 @@ import wget
 
 
 def randomSeqGen(n):
-    dnaMolecule='ATGC'
+    dnaMolecule = 'ATGC'
     seq = ''
     for _ in range(n):
         # random.seed(8)
@@ -13,13 +13,15 @@ def randomSeqGen(n):
     # print(seq)
     return seq
 
-def longestCommonPrefix(s1,s2):
-    i=0
-    while i<len(s1) and i <len(s2) and s1[i] == s2[i]:
-        i+=1
+
+def longestCommonPrefix(s1, s2):
+    i = 0
+    while i < len(s1) and i < len(s2) and s1[i] == s2[i]:
+        i += 1
     return s1[:i]
 
-def matchVerification(s1,s2):
+
+def matchVerification(s1, s2):
     if not len(s1) == len(s2):
         return False
     for i in range(len(s1)):
@@ -27,12 +29,14 @@ def matchVerification(s1,s2):
             return False
     return True
 
+
 def reverseComplement(s):
-    complement ={'A':'T','C':'G', 'G':'C', 'T':'A'}
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     t = ''
     for base in s:
         t = complement[base] + t
     return t
+
 
 def readGenome(filename):
     genome = ''
@@ -44,9 +48,10 @@ def readGenome(filename):
         for line in f:
             # ignore header line with genome information
             if not line[0] == '>':
-                #rstrip removes trailing white space from the ends of the string. in this case it will trip off the new line at the end
+                # rstrip removes trailing white space from the ends of the string. in this case it will trip off the new line at the end
                 genome += line.rstrip()
     return genome
+
 
 def main():
     random.seed(13)
@@ -56,19 +61,19 @@ def main():
     # random.seed(13)
     seq2 = ''.join([random.choice('ATGC') for _ in range(10)])
     print(seq2)
-#     These 2 methods(inline and method) will generate same 'pattern' of the sequence due to the same seed.
-#     CATTTAGCGT
-#     Systematically, as long as I don't give new seed value, the 'patter' will continue.
-    commonPrefix = longestCommonPrefix('GGTTTTTTAT','GGTTTAAAAAAA')
+    #     These 2 methods(inline and method) will generate same 'pattern' of the sequence due to the same seed.
+    #     CATTTAGCGT
+    #     Systematically, as long as I don't give new seed value, the 'patter' will continue.
+    commonPrefix = longestCommonPrefix('GGTTTTTTAT', 'GGTTTAAAAAAA')
     print(commonPrefix)
 
-    matchBool1 = matchVerification(seq2,seq2)
-    matchBool2 = matchVerification(seq1,seq2)
+    matchBool1 = matchVerification(seq2, seq2)
+    matchBool2 = matchVerification(seq1, seq2)
     print(matchBool1)
     print(matchBool2)
 
-# reverseComplement - use Dictionary
-    complement = {'A':'T','C':'G', 'G':'C', 'T':'A'}
+    # reverseComplement - use Dictionary
+    complement = {'A': 'T', 'C': 'G', 'G': 'C', 'T': 'A'}
     print(complement['C'])
     complementMethod = reverseComplement(seq1)
     # imagine there is a double stranded DNA,
@@ -79,19 +84,20 @@ def main():
 
     url = 'http://d28rh4a8wq0iu5.cloudfront.net/ads1/data/lambda_virus.fa'
     # filename = wget.download(url)
-    filename='lambda_virus.fa'
+    filename = 'lambda_virus.fa'
     print(filename)
     genome = readGenome('lambda_virus.fa')
     print(genome[:100])
     print(len(genome))
-    counts = {'A':0, 'T':0, 'G':0,'C':0}
+    counts = {'A': 0, 'T': 0, 'G': 0, 'C': 0}
     for base in genome:
-        counts[base]+=1
+        counts[base] += 1
     print(counts)
     # {'A': 12334, 'T': 11986, 'G': 12820, 'C': 11362}
     import collections
     cc = collections.Counter(genome)
     print(cc)
+
 
 if __name__ == '__main__':
     main()
